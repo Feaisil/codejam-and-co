d03ddf170b16822b69aca249dc65334ed8920713
Paris MEULEMAN <paris.meuleman@gmail.com>
2017-02-06 02:30:44 +1100

    2016 round 1B pbs a, b, c

---
+++ b.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+template<typename T>
+void r(T& o)
+{
+	cin >> o;
+}
+template<typename T>
+void rv( int n, vector<T> &o) 
+{
+	for(int bal=0; bal<n;bal++)
+	{
+		int j=0;
+		r(j);
+		o.push_back(j);
+	}
+}
+template<typename T>
+void rv(vector<T> &o)
+{
+	int t;
+	r(t);
+	rv(t,o);
+}
+
+template<typename T>
+void ov(const vector<T> &i)
+{
+	for(auto v: i)
+	{
+		cout << v << " ";
+	}
+}
+
+void proc()
+{
+	vector<int> a;
+	rv(a);
+
+	ov(a);
+}
+
+int main()
+{
+	int t;
+	int n;
+	int j;
+	r(t);
+	cerr << "Going for " << t << " cases" << endl;
+	for(int i = 1; i<t+1; i++)
+	{
+		cerr << "Starting case " << i << endl;
+		cout << "Case #" << i << ":";
+		proc();
+		cout << endl; 
+	}
+	return 0;
+}
